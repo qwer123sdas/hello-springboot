b0555e8e115640c3e0a3d732e36e435b32eb3052
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-07-15 12:58:38 +0900

    first commit

---
+++ src/test/java/com/travelclub/homework/HellowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelclub.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
