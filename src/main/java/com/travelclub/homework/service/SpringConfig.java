3aba416d37cd911f56b5f628c3e1b78f7e89be86
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-07-19 20:16:15 +0900

    Aop

--- src/main/java/com/travelclub/homework/service/SpringConfig.java
+++ src/main/java/com/travelclub/homework/service/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.travelclub.homework.aop.TimeTraceAop;
 import com.travelclub.homework.repository.JdbcMemberRepository;
 import com.travelclub.homework.repository.JdbcTemplateMemberRepository;
 import com.travelclub.homework.repository.JpaMemberRepository;
@@ -36,6 +37,7 @@ public class SpringConfig {
 		return new MemberService(memberRepository); // 스프링 빈을 등록하고, 등록되어 잇는 레파지토리를
 		// 멤버 서비스에 넣어준다. // 해당 메소드를 리턴했기 떄문에 그 리턴값인 빈을 넣어주게 된다.
 	}
+	
 	
 	/*
 	@Bean
